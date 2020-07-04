91f4faaafbfa2422147de75dbb564f1d1cbe3106
mtbfilho <mtbfilho@gmail.com>
2020-07-04 09:16:30 -0300

    pods added

--- CVChallenge/CVChallenge/ViewController.swift
+++ CVChallenge/CVChallenge/ViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCatImages()
+    }
+    
+    func loadCatImages() {
+        let headers: HTTPHeaders = [
+            "Authorization": "Client-ID 1ceddedc03a5d71"
+        ]
+
+        Alamofire.request("https://api.imgur.com/3/gallery/search/?q=cats", headers: headers).responseJSON { response in
+            debugPrint(response)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
